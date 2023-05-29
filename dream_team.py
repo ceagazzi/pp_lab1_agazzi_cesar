0ececfaa8f3d09138b69db0072a69788ef8d8982
ceagazzi <ceagazzi@hotmail.com>
2023-05-29 18:03:26 -0300

    Pacial finalizado de manera honesta

--- dream_team.py
+++ dream_team.py
@@ -4,20 +4,79 @@ from functions import*
 
 player_list = parse_json("/Users/CEA/Programacion/pp_lab1_agazzi_cesar/dt.json")
 finish = True
+flag_case = 0
 while(finish):
     answer = int(input("\n\nPor favor ingrese una opción:\n"
                         "1-Mostrar la lista de todos los jugadores del Dream Team.\n"
                         "2-Mostrar estadisticas completas de un jugador:\n"
-                        "3-Generar archivo CSV del jugador elegido"))
+                        "3-Generar archivo CSV del jugador elegido.\n"
+                        "4-Buscar jugador y mostrar sus logros.\n"
+                        "5-Calcular y mostrar el promedio de puntos por partido de todo el equipo del Dream Team.\n"
+                        "6-Buscar jugador y mostrar si es miembro del salon de la fama.\n"
+                        "7-Calcular y mostrar el jugador con la mayor cantidad de rebotes totales.\n"
+                        "8-Calcular y mostrar el jugador con el mayor porcentaje de tiros de campo.\n"
+                        "9-Calcular y mostrar el jugador con la mayor cantidad de asistencias totales.\n"
+                        "10-Ingresar un valor y mostrar los jugadores que han promediado más puntos por partido que ese valor.\n"
+                        "11-Ingresar un valor y mostrar los jugadores que han promediado más rebotes por partido que ese valor.\n"
+                        "12-Ingresar un valor y mostrar los jugadores que han promediado más rebotes por partido que ese valor.\n"
+                        "13-Calcular y mostrar el jugador con la mayor cantidad de robos totales.\n"
+                        "14-Calcular y mostrar el jugador con la mayor cantidad de bloqueos totales.\n"
+                        "15-Ingresar un valor y mostrar los jugadores que hayan tenido un porcentaje de tiros libres superior a ese valor\n"
+                        "16-Calcular y mostrar el promedio de puntos por partido del equipo excluyendo al jugador con la menor cantidad de puntos por partido.\n"
+                        "17-Calcular y mostrar el jugador con la mayor cantidad de logros obtenidos.\n"
+                        "18-Ingresar un valor y mostrar los jugadores que hayan tenido un porcentaje de tiros triples superior a ese valor.\n"
+                        "19-Calcular y mostrar el jugador con la mayor cantidad de temporadas jugadas.\n"
+                        "20-Ingresar un valor y mostrar los jugadores , ordenados por posición en la cancha, que hayan tenido un porcentaje de tiros de campo superior a ese valor.\n"
+                        "21-Salir del programa"))
      
     match(answer):
         case 1:
             print_player_position(player_list)
-        
         case 2:
              print_player(player_list)
              answer_player = int(input("\nSeleccione un jugador de la lista:"))
              print_complete_statistics(player_list,answer_player)
-
+             flag_case = 1
         case 3:
-            generate_player_csv("jugador.csv", player_list, answer_player)
+            if flag_case == 0:
+                print("Primero debe seleccionar la opcion 2\n")
+            else:                    
+                generate_player_csv("jugador.csv", player_list, answer_player)
+                print("\nEl archivo CSV del jugador elegido fue generado correctamente\n")        
+        case 4:
+            search_player_by_name(player_list)
+        case 5:
+            pass
+        case 6:
+            pass
+        case 7:
+            calculate_and_show_most_rebounds(player_list)
+        case 8:
+            calculate_and_show_max_percentage_field_goals(player_list)
+        case 9:
+            calculate_and_show_most_assists(player_list)
+        case 10:
+            print(show_players_average_points_comparison(player_list))
+        case 11:
+            print(show_players_average_rebounds_comparison(player_list))
+        case 12:
+            print(show_players_average_assists_comparison(player_list))
+        case 13:
+            calculate_and_show_highest_total_robberies(player_list)
+        case 14:
+            calculate_and_show_highest_total_blocks(player_list)
+        case 15:
+            print(show_players_free_throw_percentage_comparison(player_list))
+        case 16:
+            calculate_and_show_ppg_average(player_list)
+        case 17:
+            calculate_and_show_highest_prizes(player_list)
+        case 18:
+            show_players_3points_shooting_percentage_comparison(player_list)
+        case 19:
+            calculate_and_show_most_seasons(player_list)
+        case 20:
+            pass
+        case 21:
+            finish = False
+            print("\n***** Usted ha salido del programa *****\n")
--- functions.py
+++ functions.py
@@ -34,22 +34,173 @@ def print_complete_statistics(una_lista:list, indice:int):
             una_lista[indice]["estadisticas"]["porcentaje_tiros_libres"],
             una_lista[indice]["estadisticas"]["porcentaje_tiros_triples"]))
 
-#lo armo con lista e indice, pero deberia probar de guardar un jugador como dict y pasarlo como parametro
-#def generate_player_csv(file_name:str, player:dict):
 def generate_player_csv(file_name:str, una_lista:list, indice:int):
     with open(file_name, "w") as file:
         text = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}\n".format(una_lista[indice]["nombre"],
-                                                                   una_lista[indice]["posicion"],
-                                                                   una_lista[indice]["estadisticas"]["temporadas"],
-                                                                   una_lista[indice]["estadisticas"]["puntos_totales"],
-                                                                   una_lista[indice]["estadisticas"]["promedio_puntos_por_partido"],
-                                                                   una_lista[indice]["estadisticas"]["rebotes_totales"],
-                                                                   una_lista[indice]["estadisticas"]["promedio_rebotes_por_partido"],
-                                                                   una_lista[indice]["estadisticas"]["asistencias_totales"],
-                                                                   una_lista[indice]["estadisticas"]["promedio_asistencias_por_partido"],
-                                                                   una_lista[indice]["estadisticas"]["robos_totales"],
-                                                                   una_lista[indice]["estadisticas"]["bloqueos_totales"],
-                                                                   una_lista[indice]["estadisticas"]["porcentaje_tiros_de_campo"],
-                                                                   una_lista[indice]["estadisticas"]["porcentaje_tiros_libres"],
-                                                                   una_lista[indice]["estadisticas"]["porcentaje_tiros_triples"],)
+                  una_lista[indice]["posicion"],
+                  una_lista[indice]["estadisticas"]["temporadas"],
+                  una_lista[indice]["estadisticas"]["puntos_totales"],
+                  una_lista[indice]["estadisticas"]["promedio_puntos_por_partido"],
+                  una_lista[indice]["estadisticas"]["rebotes_totales"],
+                  una_lista[indice]["estadisticas"]["promedio_rebotes_por_partido"],
+                  una_lista[indice]["estadisticas"]["asistencias_totales"],
+                  una_lista[indice]["estadisticas"]["promedio_asistencias_por_partido"],
+                  una_lista[indice]["estadisticas"]["robos_totales"],
+                  una_lista[indice]["estadisticas"]["bloqueos_totales"],
+                  una_lista[indice]["estadisticas"]["porcentaje_tiros_de_campo"],
+                  una_lista[indice]["estadisticas"]["porcentaje_tiros_libres"],
+                  una_lista[indice]["estadisticas"]["porcentaje_tiros_triples"],)
         file.write(text)
+
+def search_player_by_name(una_lista:list):
+    player_name_list = []
+    name = input("Ingrese el nombre del jugador:\n")
+    pattern = r".*" + name + r".*"
+    for player in una_lista:
+        if re.search(pattern, player["nombre"]):
+            player_name_list.append(player["logros"])
+    print(player_name_list)
+    return player_name_list
+
+# def search_player_by_name_hall_of_fame(una_lista:list):
+#     player_name_list = []
+#     name = input("Ingrese el nombre del jugador:\n")
+#     pattern = r".*" + name + r".*"
+#     for player in una_lista:
+#         if re.search(pattern, player["nombre"]):
+#             if player["logros"] == "Miembro del Salon de la Fama del Baloncesto":
+#                 player_name_list.append(player["logros"])
+#     print(player_name_list)
+#     return player_name_list
+
+def calculate_and_show_most_rebounds(una_lista:list):
+    max_rebounds = una_lista[0]["estadisticas"]["rebotes_totales"]
+    player_max_rebounds = una_lista[0]["nombre"]
+    for player in una_lista:
+        if player["estadisticas"]["rebotes_totales"] > max_rebounds:
+            max_rebounds = player["estadisticas"]["rebotes_totales"]
+            player_max_rebounds = player["nombre"]
+    print("El jugador con mas rebotes ({0}) es {1}".format((max_rebounds),(player_max_rebounds)))
+
+def calculate_and_show_max_percentage_field_goals(una_lista:list):
+    max_field_goals = una_lista[0]["estadisticas"]["porcentaje_tiros_de_campo"]
+    player_max_field_goals = una_lista[0]["nombre"]
+    for player in una_lista:
+        if player["estadisticas"]["porcentaje_tiros_de_campo"] > max_field_goals:
+            max_field_goals = player["estadisticas"]["porcentaje_tiros_de_campo"]
+            player_max_field_goals = player["nombre"]
+    print("El jugador con el mayor promedio de tiros de campo ({0}) es {1}".format((max_field_goals),(player_max_field_goals)))
+
+def calculate_and_show_most_assists(una_lista:list):
+    most_assists = una_lista[0]["estadisticas"]["asistencias_totales"]
+    player_most_assists = una_lista[0]["nombre"]
+    for player in una_lista:
+        if player["estadisticas"]["asistencias_totales"] > most_assists:
+            most_assists = player["estadisticas"]["asistencias_totales"]
+            player_most_assists = player["nombre"]
+    print("El jugador con el mas asistencias  ({0}) es {1}".format((most_assists),(player_most_assists)))
+
+def calculate_and_show_highest_total_robberies(una_lista:list):
+    highest_total_robberies = una_lista[0]["estadisticas"]["robos_totales"]
+    player_highest_total_robberies = una_lista[0]["nombre"]
+    for player in una_lista:
+        if player["estadisticas"]["robos_totales"] > highest_total_robberies:
+            highest_total_robberies = player["estadisticas"]["robos_totales"]
+            player_highest_total_robberies = player["nombre"]
+    print("El jugador con mas cantidad de robos totales ({0}) es {1}".format((highest_total_robberies),(player_highest_total_robberies)))
+
+def calculate_and_show_highest_total_blocks(una_lista:list):
+    highest_total_blocks = una_lista[0]["estadisticas"]["bloqueos_totales"]
+    player_highest_total_blocks = una_lista[0]["nombre"]
+    for player in una_lista:
+        if player["estadisticas"]["bloqueos_totales"] > highest_total_blocks:
+            highest_total_blocks = player["estadisticas"]["bloqueos_totales"]
+            player_highest_total_blocks = player["nombre"]
+    print("El jugador con mas cantidad de bloqueos totales ({0}) es {1}".format((highest_total_blocks),(player_highest_total_blocks)))
+
+def calculate_and_show_ppg_average(una_lista:list):
+    points_per_game = 0.00
+    ppg_average = 0.00
+    players = 0
+    less_points = una_lista[0]["estadisticas"]["promedio_puntos_por_partido"]
+    for player in una_lista:
+        players = players + 1
+        points_per_game = points_per_game + player["estadisticas"]["promedio_puntos_por_partido"]
+        if player["estadisticas"]["promedio_puntos_por_partido"] < less_points:
+            less_points = player["estadisticas"]["promedio_puntos_por_partido"]
+    print(points_per_game)
+    points_per_game = points_per_game - less_points
+    print(points_per_game)
+    players = players - 1
+    print(players)
+    ppg_average = points_per_game / players
+    print("El promedio de puntos por partido del equipo (exlcuido aquel con la menor cantidad) es {0}".format((ppg_average)))
+
+def calculate_and_show_highest_prizes(una_lista:list):
+    highest_prizes = 0
+    player_prizes = ""
+    for player in una_lista:
+        count_prizes = len(player["logros"])
+        if count_prizes > highest_prizes:
+            highest_prizes = count_prizes
+            player_prizes = player["nombre"]
+    print("El jugador con mas cantidad de premios ({0}) es {1}".format((highest_prizes),(player_prizes)))
+
+def calculate_and_show_most_seasons(una_lista:list):
+    most_seasons = una_lista[0]["estadisticas"]["temporadas"]
+    player_most_seasons = una_lista[0]["nombre"]
+    for player in una_lista:
+        if player["estadisticas"]["temporadas"] > most_seasons:
+            most_seasons = player["estadisticas"]["temporadas"]
+            player_most_seasons = player["nombre"]
+    print("El jugador con mas cantidad de temporadas jugadas ({0}) es {1}".format((most_seasons),(player_most_seasons)))
+
+def show_players_average_points_comparison(una_lista:list):
+    comparison = int(input("Por favor ingrese un valor de puntos por partido:\n"))
+    print("Puntos a comparar", comparison)
+    players = []
+    for player in una_lista:
+        if player["estadisticas"]["promedio_puntos_por_partido"] > comparison:
+            players.append(player["nombre"])
+    for player in players:
+        print(player)
+
+def show_players_average_rebounds_comparison(una_lista:list):
+    comparison = int(input("Por favor ingrese un valor de rebotes por partido:\n"))
+    print("Rebotes a comparar", comparison)
+    players = []
+    for player in una_lista:
+        if player["estadisticas"]["promedio_rebotes_por_partido"] > comparison:
+            players.append(player["nombre"])
+    for player in players:
+        print(player)
+
+def show_players_average_assists_comparison(una_lista:list):
+    comparison = int(input("Por favor ingrese un valor de asistencias por partido:\n"))
+    print("Asistencias a comparar", comparison)
+    players = []
+    for player in una_lista:
+        if player["estadisticas"]["promedio_asistencias_por_partido"] > comparison:
+            players.append(player["nombre"])
+    for player in players:
+        print(player)
+
+def show_players_free_throw_percentage_comparison(una_lista:list):
+    comparison = int(input("Por favor ingrese un porcentaje de tiros libres por partido:\n"))
+    print("Porcentaje de tiros libres a comparar", comparison)
+    players = []
+    for player in una_lista:
+        if player["estadisticas"]["porcentaje_tiros_libres"] > comparison:
+            players.append(player["nombre"])
+    for player in players:
+        print(player)
+
+def show_players_3points_shooting_percentage_comparison(una_lista:list):
+    comparison = int(input("Por favor ingrese un porcentaje de tiros triples por partido:\n"))
+    print("Porcentaje de tiros triples a comparar\n", comparison)
+    players = []
+    for player in una_lista:
+        if player["estadisticas"]["porcentaje_tiros_triples"] > comparison:
+            players.append(player["nombre"])
+    for player in players:
+        print(player)
